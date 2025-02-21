315b4ff6003fa76f85bb1e0127921a1a3165d28d
Petriv2004 <felipetrivinogarzon@gmail.com>
2025-02-21 14:56:28 -0500

    Subiendo archivos

---
+++ app/src/main/java/com/example/activitieseintents/ReceiveMessageActivity.java
@@ -0,0 +1,32 @@
+package com.example.activitieseintents;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_receive_message);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView) findViewById(R.id.message);
+        messageView.setText(messageText);
+    }
+}
